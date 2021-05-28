425668b3045be4b72240ac346c62ee791ca65c36
Peter Pauly <p.pauly@tu-bs.de>
2021-05-28 16:40:56 +0200

    adds first fields to GUI

--- src/config_generator.py
+++ src/config_generator.py
@@ -1,7 +1,10 @@
 """ generate a config if none found """
 import tkinter as tk
+from tkinter import ttk
+from tkcalendar import DateEntry
 
 def start_config_generation():
+    """ entry point for config generation """
     run_gui = True
     try:
         gui_window = tk.Tk()
@@ -15,7 +18,33 @@ def start_config_generation():
         run_cli_config()
 
 def run_gui_config(tk_window):
-    return 0
+    """ create a window for data entry """
+    tk_window.geometry("400x400+250+100")
+
+    row_index = 6
+    notificators = ["Mail", "Telegram", "Browser"]
+    checkboxes = {}
+    enable = {}
+
+    tk.Label(tk_window, text="Geburtsdatum", font="bold").grid(row=1, column=1)
+    tk.Label(tk_window, text="PLZ", font="bold").grid(row=3, column=1)
+
+    birthday = DateEntry(tk_window, width=18)
+    plz = tk.Entry(tk_window)
+
+    birthday.grid(row=2, column=1)
+    plz.grid(row=4, column=1)
+
+    tk.Label(tk_window, text="Benachrichtigung", font="bold").grid(row=5, column=1)
+    for item in notificators:
+        enable[item] = tk.BooleanVar()
+        checkboxes[item] = ttk.Checkbutton(tk_window, text=item, variable=enable[item], onvalue=True)
+        checkboxes[item].grid(row=row_index, column=1)
+        row_index += 1
+
+    tk_window.columnconfigure(0, weight=1)
+    tk_window.columnconfigure(2, weight=1)
+    tk_window.mainloop()
 
 def run_cli_config():
     return 0
