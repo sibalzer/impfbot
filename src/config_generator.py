dcbbf416aa636b3783cb5367b4bce2015ac38756
Peter Pauly <p.pauly@tu-bs.de>
2021-05-29 10:32:02 +0200

    adds saving config to file

--- src/config_generator.py
+++ src/config_generator.py
@@ -4,6 +4,19 @@ import configparser
 from tkinter import ttk
 from tkcalendar import DateEntry
 
+NOTIFICATORS = ["EMail", "Telegram", "Webbrowser"]
+FIELDS = {
+    "EMail": {"sender": "Empfänger",
+        "password": "Passwort",
+        "server": "Server",
+        "port": "Port",
+        "receivers": "Empfänger"
+    },
+    "Telegram": {"token": "Token",
+        "chat_ids": "Chat-IDs"
+    }
+}
+
 def start_config_generation(config_dict):
     """ entry point for config generation """
     run_gui = True
@@ -18,41 +31,35 @@ def start_config_generation(config_dict):
     else:
         run_cli_config()
 
-
-
 def run_gui_config(tk_window, config_dict):
     """ create a window for data entry """
-    def get_input():
+    def init_input():
         config_dict["COMMON"] = {}
+        for item in NOTIFICATORS:
+            config_dict[item.upper()] = {}
+
+    def get_input():
         config_dict["COMMON"]["geburtstag"] = birthday.get()
         config_dict["COMMON"]["postleitzahl"] = plz.get()
-        for item in notificators:
-            config_dict[item.upper()] = {}
+        for item in NOTIFICATORS:
             config_dict[item.upper()]["enable"] = str(enable[item].get()).lower()
         with open("test.ini", "w") as configfile:
             config_dict.write(configfile)
 
     def create_subwindow(event):
+        """ when selecting a notificator, show a new window with the required options """
         def close_subwindow():
+            for field in FIELDS[notificator]:
+                config_dict[notificator.upper()][field] = input_arr[field].get()
             subwindow.destroy()
+
         notificator = event.widget.cget("text")
         if notificator != "Webbrowser" and not enable[notificator].get():
             enable[notificator].set(not enable[notificator].get())
-            fields = {
-                "EMail": {"sender": "Empfänger",
-                    "password": "Passwort",
-                    "server": "Server",
-                    "port": "Port",
-                    "receivers": "Empfänger"
-                },
-                "Telegram": {"token": "Token",
-                    "chat_ids": "Chat-IDs"
-                }
-            }
             input_arr = {}
             subwindow = tk.Toplevel(tk_window)
             row_index = 0
-            notificator_fields = fields[notificator]
+            notificator_fields = FIELDS[notificator]
             for field in notificator_fields:
                 tk.Label(subwindow, text=notificator_fields[field]).grid(row=row_index, column=0)
                 input_arr[field] = tk.Entry(subwindow)
@@ -63,12 +70,13 @@ def run_gui_config(tk_window, config_dict):
             close.grid(row=row_index, column=1)
 
     def close_window():
+        get_input()
         tk_window.destroy()
 
+    init_input()
     tk_window.geometry("400x400+250+100")
 
     row_index = 6
-    notificators = ["EMail", "Telegram", "Webbrowser"]
     checkboxes = {}
     enable = {}
 
@@ -82,7 +90,7 @@ def run_gui_config(tk_window, config_dict):
     plz.grid(row=4, column=1)
 
     tk.Label(tk_window, text="Benachrichtigung", font="bold").grid(row=5, column=1)
-    for item in notificators:
+    for item in NOTIFICATORS:
         enable[item] = tk.BooleanVar()
         enable[item].set(False)
         checkboxes[item] = ttk.Checkbutton(
