e98f18eae18e20ff2f4abaf5380fab0e3074cdb3
sibalzer <github@simonbalzer.de>
2021-05-31 13:40:37 +0200

    add common test

--- src/common.py
+++ src/common.py
@@ -9,11 +9,17 @@ def sleep(time_in_s: int, jitter: int = 0) -> None:
     start = datetime.now()
     end = datetime.now() + timedelta(seconds=time_to_wait)
     while end > datetime.now():
-        elapsed_time = datetime.now() - start
-        time_str = f"\rSleeping ({int(elapsed_time.total_seconds())}s/{int(time_to_wait)}s)"
+        elapsed_time = (datetime.now() - start).total_seconds()
+        elapsed_time_delta = time_in_s - elapsed_time
+
+        time_str = f"\rSleeping ({int(elapsed_time)}s/{int(time_to_wait)}s)"
         sys.stdout.write(time_str)
         sys.stdout.flush()
-        time.sleep(1)
+
+        if elapsed_time_delta < 1:
+            time.sleep(elapsed_time_delta)
+        else:
+            time.sleep(1)
     sys.stdout.write("\r")
     sys.stdout.flush()
 
