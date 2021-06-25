f6461faa3340cb82ce4a4b5277eed3a09acf4f46
sibalzer <github@simonbalzer.de>
2021-06-26 01:41:03 +0200

    tests: fix warning call count

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -144,7 +144,7 @@ def test_old(log_info_mock,
 
     # Check output
     assert log_info_mock.call_count == 1
-    assert log_warning_mock.call_count == 12
+    assert log_warning_mock.call_count == 13
     for msg in log_warning_mock.call_args_list:
         assert (" is depracated please use: " in msg.args[0]
                 or "[EMAIL] 'user' is missing; set sender as user" in msg.args[0]
@@ -195,7 +195,7 @@ def test_missing_optional(log_info_mock,
 
     # Check output
     assert log_info_mock.call_count == 4
-    assert log_warning_mock.call_count == 8
+    assert log_warning_mock.call_count == 9
     log_error_mock.assert_not_called()
 
     # EMAIL
